948d921c1bccbfe6ef816b82e12d6f80c547fc66
Thales <thalesreis.es@gmail.com>
2025-07-29 16:53:48 -0300

    Fazendo exercicios de condicionais

---
+++ Modulo_D/exer01.js
@@ -0,0 +1,8 @@
+var velocidade = 61;
+console.log("A sua velocidade é " + velocidade + " km/h");
+
+if(velocidade > 60){
+    console.log("Você está acima da velocidade permitida!");
+}else{
+    console.log("Você está dentro da velocidade permitida!");
+}
