c5639570c192bb1901f3c238d01e76ee9d3ab1e4
Thales <thalesreis.es@gmail.com>
2025-07-30 19:14:37 -0300

    Terminando o a etapa de condicionais, iniciando os exercícios práticos

---
+++ Modulo_D/exer04.js
@@ -0,0 +1,11 @@
+//IF else + else if
+
+var nota = 6;
+
+if (nota >= 9) {
+    console.log("Aprovado com louvor");
+}else if (nota >= 7) {
+    console.log("Aprovado");
+}else {
+    console.log("Reprovado");
+}
